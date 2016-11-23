471508f272f8b9346404ba496a32407e520c5432
michfarr <etnunc@protonmail.com>
2016-11-23 22:59:31 +0100

    Fibonacci with tests.  Added multiplier function.

--- lib/calculations.ex
+++ lib/calculations.ex
@@ -1,3 +1,7 @@
 defmodule Calculations do
   def square(n), do: n*n
+
+  def multiplier(n), do: fn (x) -> x * n end
+
+  def fibonacci(n), do: if n <= 1, do: 1, else: fibonacci(n-2) + fibonacci(n-1)
 end
--- test/calculations_test.exs
+++ test/calculations_test.exs
@@ -3,7 +3,25 @@ defmodule CalculationsTest do
   doctest Calculations
 
   test "Calculations.square/1" do
-    assert 4 = Calculations.square(2)
-    assert 16 = Calculations.square(4)
+    assert 4 == Calculations.square(2)
+    assert 16 == Calculations.square(4)
+  end
+
+  test "Calcuations.multiplier/1, double" do
+    double = Calculations.multiplier(2)
+    assert 8 == double.(4)
+  end
+
+  test "Calculations.multiplier/1, triple" do
+    triple = Calculations.multiplier(3)
+    assert 27 = triple.(9)
+  end
+
+  test "Calculations.fibonacci/1" do
+    assert 1 == Calculations.fibonacci(0)
+    assert 1 == Calculations.fibonacci(1)
+    assert 2 == Calculations.fibonacci(2)
+    assert 3 == Calculations.fibonacci(3)
+    assert 5 == Calculations.fibonacci(4)
   end
 end
